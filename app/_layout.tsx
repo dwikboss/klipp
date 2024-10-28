3b27436a0aa2ffcef7f22acaee4421fcaad71233
dwikboss <dwikboss99@gmail.com>
2024-10-28 18:14:24 +0100

    feat: create basic home view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,20 @@
-import { Stack } from "expo-router";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
 
-export default function RootLayout() {
+const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+    <Slot/>
+  )
 }
+
+export default RootLayout
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
